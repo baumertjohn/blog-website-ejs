5a69464eb6663fbb85d60b20f3926fbb0998a181
John Baumert <36886175+baumertjohn@users.noreply.github.com>
2022-12-10 17:20:41 -0700

    Add dynamic URL to home page and dyanmic text to post page.

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const aboutContent =
 const contactContent =
   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
+let posts = [];
+
 const app = express();
 
 app.set("view engine", "ejs");
@@ -17,7 +19,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  res.render("home", { homeStartingContent: homeStartingContent });
+  res.render("home", {
+    homeStartingContent: homeStartingContent,
+    posts: posts,
+  });
 });
 
 app.get("/about", function (req, res) {
@@ -33,9 +38,16 @@ app.get("/compose", function (req, res) {
 });
 
 app.post("/compose", function (req, res) {
-  console.log(req.body.postTitle);
-  console.log(req.body.postBody);
-  res.render("compose");
+  let post = {
+    title: req.body.postTitle,
+    content: req.body.postBody,
+  };
+  posts.push(post);
+  res.redirect("/");
+});
+
+app.get("/post/:postNum", function (req, res) {
+  res.render("post", { post: posts[req.params.postNum] });
 });
 
 app.listen(3000, function () {
